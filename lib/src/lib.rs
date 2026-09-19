b8f2c68cffee08d30f88a30b68066b1d669b0d6a
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-09-20 08:53:43 +0900

    first commit

---
+++ lib/src/lib.rs
@@ -0,0 +1,33 @@
+#![allow(unused)]
+
+use nannou::prelude::*;
+
+pub struct Model {
+    window: Entity,
+    pub was_updated: bool,
+    state: State,
+}
+
+impl Model {
+    pub fn for_window(window: Entity) -> Self {
+        Self {
+            window,
+            state: State::default(),
+            was_updated: false,
+        }
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct State {}
+
+#[unsafe(no_mangle)]
+pub fn update(app: &App, model: &mut Model) {
+
+}
+
+#[unsafe(no_mangle)]
+pub fn view(app: &App, model: &Model) {
+    let draw = app.draw();
+    draw.background().color(WHITE);
+}
